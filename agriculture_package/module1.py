206e49e7d4f0ac2d74b6f9d41e20db33825a389d
Muhanad Abulhusn <153107661+Muhanad-husn@users.noreply.github.com>
2024-04-03 19:06:06 +0200

    Update module1.py

--- agriculture_package/module1.py
+++ agriculture_package/module1.py
@@ -1,4 +1,8 @@
+import pandas as pd
 import seaborn as sns
+import matplotlib.pypplot as plt
+from arabic_reshaper import arabic_reshaper
+from bidi.algorithm import get_display
 
 def ar_text(text):
     """
@@ -14,8 +18,6 @@ def ar_text(text):
     Returns:
     str: The processed text string ready for display in non-Arabic script supporting environments.
     """
-    from arabic_reshaper import arabic_reshaper
-    from bidi.algorithm import get_display
     # Reshape the Arabic text to handle characters connection correctly
     reshaped_text = arabic_reshaper.reshape(text)
     # Apply the BiDi algorithm to ensure correct display order
